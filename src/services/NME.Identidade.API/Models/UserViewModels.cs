20fa272cf21f7e1f36717241f027adf06fc9792f
wilhamsJW <wilhams.dev@gmail.com>
2026-08-26 11:33:42 -0300

    feat(identidade): cria viewmodels de registro e login de usuario com validacoes

---
+++ src/services/NME.Identidade.API/Models/UserViewModels.cs
@@ -0,0 +1,29 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace NME.Identidade.API.Models
+{
+    public class UsuarioRegistro
+    {
+        [Required(ErrorMessage = "O campo {0} é obrigatório")]
+        [EmailAddress(ErrorMessage = "O campo {0} está em formato inválido")]
+        public string Email { get; set; } = string.Empty;
+
+        [Required(ErrorMessage = "O campo {0} é obrigatório")]
+        [StringLength(100, ErrorMessage = "O campo {0} precisa ter entre {2} e {1} caracteres", MinimumLength = 6)]
+        public string Senha { get; set; } = string.Empty;
+
+        [Compare("Senha", ErrorMessage = "As senhas não conferem.")]
+        public string SenhaConfirmacao { get; set; } = string.Empty;
+    }
+
+    public class UsuarioLogin
+    {
+        [Required(ErrorMessage = "O campo {0} é obrigatório")]
+        [EmailAddress(ErrorMessage = "O campo {0} está em formato inválido")]
+        public string Email { get; set; } = string.Empty;
+
+        [Required(ErrorMessage = "O campo {0} é obrigatório")]
+        [StringLength(100, ErrorMessage = "O campo {0} precisa ter entre {2} e {1} caracteres", MinimumLength = 6)]
+        public string Senha { get; set; } = string.Empty;
+    }
+}
